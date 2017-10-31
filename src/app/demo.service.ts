b1d266e0b6d41ecac9ac9642fd8b775ba22a4da6
csoft2k <csoft2k@hotmail.com>
2017-10-31 16:16:34 +0100

    Actualizar datos demo.

--- src/app/demo.service.ts
+++ src/app/demo.service.ts
@@ -12,8 +12,8 @@ export class DemoService {
 	var datos: Elemento[] = [];
 	datos.push(new Elemento(1, "Elemento 1", 1, 1, 1));
 	datos.push(new Elemento(2, "Elemento 2", 5, 2, 10));
-	datos.push(new Elemento(3, "Elemento 2", 10, 14, 140));
-	datos.push(new Elemento(4, "Elemento 2", 20, 5, 100));
+	datos.push(new Elemento(3, "Elemento 3", 10, 14, 140));
+	datos.push(new Elemento(4, "Elemento 4", 20, 5, 100));
 	
 	return JSON.stringify(datos);
   }
